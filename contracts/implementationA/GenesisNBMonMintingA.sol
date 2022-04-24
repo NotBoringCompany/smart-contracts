f10be1ee4882b9ca8d5c5785d67663914723ae1a
juandogels <suwandresukijat@gmail.com>
2022-04-24 21:50:13 +0200

    implemented hatching egg security

--- contracts/implementationA/GenesisNBMonMintingA.sol
+++ contracts/implementationA/GenesisNBMonMintingA.sol
@@ -10,12 +10,14 @@ contract GenesisNBMonMintingA is GenesisNBMonCoreA, ReentrancyGuard {
         // this base URI will only be temporary. it will change during proper deployment.
         setBaseURI("https://marketplace.nbcompany.io/nbmons/genesis/");
         _mintingAllowed = true;
+        _hatchingAllowed = true;
         // 0.15 ETH
         mintingPrice = 0.15 * 10 ** 18;
         mintLimit = 1;
         //total = 5000
         devMintLimit = 350;
-        publicSupplyLimit = 4580;
+        publicSupplyLimit = 5000;
+        // for KOLs, partnerships etc.
         adoptionIncentivesSupplyLimit = 70;
     }
 
@@ -44,6 +46,69 @@ contract GenesisNBMonMintingA is GenesisNBMonCoreA, ReentrancyGuard {
     function disallowMinting() public whenMintingAllowed onlyAdmin {
         _mintingAllowed = false;
         emit MintingNotAllowed(_msgSender());
+    }
+    
+    /**
+     * @dev Start of egg hatching security implementation
+     */
+
+    /// @dev HatchingStats will be obtained from our backend API and will be passed on for hatching.
+    struct HatchingStats {
+        bool exists;
+        string[] nbmonStats;
+        string[] types;
+        uint8[] potential;
+        string[] passives;
+    }
+
+    event HatchingAllowed(address _owner);
+    event HatchingNotAllowed(address _owner);
+
+    modifier whenHatchingAllowed() {
+        require(_hatchingAllowed, "GenesisNBMonMintingA: Hatching enabled.");
+        _;
+    }
+
+    modifier whenHatchingNotAllowed() {
+        require(!_hatchingAllowed, "GenesisNBMonMintingA: Hatching disabled.");
+        _;
+    }
+
+    function allowHatching() public whenHatchingNotAllowed onlyAdmin {
+        _hatchingAllowed = true;
+        emit HatchingAllowed(_msgSender());
+    }
+
+    function disallowHatching() public whenHatchingAllowed onlyAdmin {
+        _hatchingAllowed = false;
+        emit HatchingNotAllowed(_msgSender());
+    }
+
+    bool public _hatchingAllowed;
+
+    /// @dev This string will be encrypted and will be used as a passcode for hatching.
+    mapping (string => HatchingStats) private isValidKey;
+
+    /// checks if key specified exists within the isValidKey mapping
+    function checkValidKey(string memory _key) public view onlyAdmin returns (bool) {
+        return isValidKey[_key].exists;
+    }
+
+    /// adds a valid key as a passcode for hatching an egg
+    function addValidKey(
+        string memory _key, 
+        string[] memory 
+        _nbmonStats, 
+        string[] memory _types, 
+        uint8[] memory _potential, 
+        string[] memory _passives
+        ) public onlyAdmin {
+            isValidKey[_key] = HatchingStats(true, _nbmonStats, _types, _potential, _passives);
+    }
+
+    /// removes a valid key once egg is hatched. will not fully delete, only will empty stats.
+    function removeValidKey(string memory _key) public onlyAdmin {
+        delete isValidKey[_key];
     }
 
     // checks whether a given address is whitelisted to mint a Genesis NBMon
@@ -206,26 +271,23 @@ contract GenesisNBMonMintingA is GenesisNBMonCoreA, ReentrancyGuard {
             currentGenesisNBMonCount += _amountToMint;
     }
 
-    function hatchFromEgg(
-        uint256 _nbmonId,
-        string[] memory _nbmonStats,
-        string[] memory _types,
-        uint8[] memory _potential,
-        string[] memory _passives
-    ) public {
-        require(_exists(_nbmonId), "GenensisNBMonMinting: Specified NBMon does not exist");
+    function hatchFromEgg(string memory _key, uint256 _nbmonId) public {
+        require(isValidKey[_key].exists == true, "GenesisNBMonMintingA: Specified key is not valid.");
+
+        HatchingStats memory _hatchingStats = isValidKey[_key];
+        require(_exists(_nbmonId), "GenesisNBMonMintingA: Specified NBMon does not exist");
         GenesisNBMon storage _genesisNBMon = genesisNBMons[_nbmonId - 1];
         
-        require(_genesisNBMon.owner == _msgSender(), "GenesisNBMonMinting: Owner does not own specified NBMon");
-        require(_genesisNBMon.isEgg == true, "GenesisNBMonMinting: NBMon is not an egg anymore");
-        require(_genesisNBMon.hatchedAt + _genesisNBMon.hatchingDuration <= block.timestamp, "GenesisNBMonMinting: Egg is not ready to hatch yet");
+        require(_genesisNBMon.owner == _msgSender(), "GenesisNBMonMintingA: Caller is not the owner of the specified NBMon.");
+        require(_genesisNBMon.isEgg == true, "GenesisNBMonMintingA: NBMon is not an egg anymore");
+        require(_genesisNBMon.hatchedAt + _genesisNBMon.hatchingDuration <= block.timestamp, "GenesisNBMonMintingA: Egg is not ready to hatch yet");
 
         //updates all the stats of the genesis NBMon
         _genesisNBMon.hatchedAt = block.timestamp;
-        _genesisNBMon.nbmonStats = _nbmonStats;
-        _genesisNBMon.types = _types;
-        _genesisNBMon.potential = _potential;
-        _genesisNBMon.passives = _passives;
+        _genesisNBMon.nbmonStats = _hatchingStats.nbmonStats;
+        _genesisNBMon.types = _hatchingStats.types;
+        _genesisNBMon.potential = _hatchingStats.potential;
+        _genesisNBMon.passives = _hatchingStats.passives;
         _genesisNBMon.isEgg = false;
     }
 
--- scripts/interact.js
+++ scripts/interact.js
@@ -7,23 +7,43 @@ async function main() {
   const genesisAAddress = '0xf113759799E2616B662F63c1cbB3821E0cb9Dddb';
   const GenesisAContract = await ethers.getContractFactory("GenesisNBMonMintingA");
   const genesisAContract = await GenesisAContract.attach(genesisAAddress);
+  
+  const moralisAPINode = process.env.MORALIS_NODEAPI;
+  const nodeURL = `https://speedy-nodes-nyc.moralis.io/${moralisAPINode}/eth/rinkeby`;
+  const customHttpProvider = new ethers.providers.JsonRpcProvider(nodeURL);
+
+  // const txHash = "0x415d35912236e643e69118e3ce396945b7409ae807f925eecb2c69fdccb599a0";
+
+  // let txReceipt = await customHttpProvider.getTransaction(txHash);
+
+  // if (txReceipt && txReceipt.blockNumber) {
+  //   if (ethers.utils.formatEther(txReceipt.value) == 0.01) {
+  //     console.log(txReceipt);
+  //   } else {
+  //     console.log("not valid");
+  //   }
+  // }
+
 
   // const signer = new ethers.Wallet(proces.env.WALLET_1, `https://speedy-nodes-nyc.moralis.io/${process.env.MORALIS_NODEAPI}/eth/rinkeby`);
 
   // let abi = 
 
   // name = NBC Experiment 1
-  let owner = "0xe253773Fdd10B4Bd9d7567e37003F7029144EF90";
-  let amountToMint = 35;
-  let hatchingDuration = 300;
-  let nbmonStats = [];
-  let types = [];
-  let potential = [];
-  let passives = [];
-  let isEgg = true;
+  // let owner = "0xe253773Fdd10B4Bd9d7567e37003F7029144EF90";
+  // let amountToMint = 1;
+  // let hatchingDuration = 300;
+  let nbmonStats = ["Male", "Common", "Fire", "Origin", "Lamox", "3000"];
+  let types = ["Electric", "Spirit"];
+  let potential = [26, 10, 2, 14, 2, 19];
+  let passives = ["Electric Sword", "Test Lol"];
+  // let isEgg = true;
 
-  const devMint = await genesisAContract.devGenesisEggMint(amountToMint, hatchingDuration, nbmonStats, types, potential, passives, isEgg);
-  console.log(devMint);
+  const hatchEgg = await genesisAContract.hatchFromEgg(102, nbmonStats, types, potential, passives);
+  console.log(hatchEgg);
+
+  // const devMint = await genesisAContract.devGenesisEggMint(amountToMint, hatchingDuration, nbmonStats, types, potential, passives, isEgg);
+  // console.log(devMint);
 
   // const ownerIds = await genesisAContract.getOwnerGenesisNBMonIds('0x460107fAB29D57a6926DddC603B7331F4D3bCA05');
   // console.log(ownerIds);
@@ -37,20 +57,24 @@ async function main() {
   // const mintWhitelist = await genesisAContract.whitelistedGenesisEggMint(owner, amountToMint, hatchingDuration, nbmonStats, types, potential, passives, isEgg);
   // console.log(mintWhitelist);
 
-  // const whitelistAddress = await genesisAContract.whitelistAddress('0x460107fAB29D57a6926DddC603B7331F4D3bCA05');
+  // const whitelistAddress = await genesisAContract.whitelistAddress('0x6ef0f724e780E5D3aD66f2A4FCbEF64A774eA796');
   // console.log(whitelistAddress);
-  // const checkWhitelist = await genesisAContract.whitelisted('0xe253773Fdd10B4Bd9d7567e37003F7029144EF90');
+  // const checkWhitelist = await genesisAContract.whitelisted('0x6ef0f724e780E5D3aD66f2A4FCbEF64A774eA796');
   // console.log(checkWhitelist);
 
   // const amountMinted = await genesisAContract.amountMinted('0xe253773Fdd10B4Bd9d7567e37003F7029144EF90');
   // console.log(amountMinted);
 
-  // const checkId = await genesisAContract.getGenesisNBMon(1);
+  // const checkId = await genesisAContract.getGenesisNBMon(101);
   // console.log(checkId);
+
+  
 
 
   // const balanceOf = await genesisAContract.balanceOf('0x6ef0f724e780E5D3aD66f2A4FCbEF64A774eA796');
   // console.log(balanceOf);
+
+
 }
 
 
